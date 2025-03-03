1c9e6d91e84d7b2b00d81c026ee960fc58652a27
Tiffany Zhang <tzhang@lcs.on.ca>
2025-03-03 14:18:23 -0500

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by xinyu zhang on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
